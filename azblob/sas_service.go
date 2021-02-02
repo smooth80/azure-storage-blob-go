4cd252f6cb8e3f464c074821b771451441c1b2cf
Narasimha Kulkarni <63087328+nakulkar-msft@users.noreply.github.com>
2021-02-02 22:38:10 +0530

    Support listing in SAS

--- azblob/sas_service.go
+++ azblob/sas_service.go
@@ -248,7 +248,7 @@ func (p *ContainerSASPermissions) Parse(s string) error {
 
 // The BlobSASPermissions type simplifies creating the permissions string for an Azure Storage blob SAS.
 // Initialize an instance of this type and then call its String method to set BlobSASSignatureValues's Permissions field.
-type BlobSASPermissions struct{ Read, Add, Create, Write, Delete, DeletePreviousVersion, Tag bool }
+type BlobSASPermissions struct{ Read, Add, Create, Write, Delete, DeletePreviousVersion, Tag, List bool }
 
 // String produces the SAS permissions string for an Azure Storage blob.
 // Call this method to set BlobSASSignatureValues's Permissions field.
@@ -274,6 +274,9 @@ func (p BlobSASPermissions) String() string {
 	}
 	if p.Tag {
 		b.WriteRune('t')
+	}
+	if p.List {
+		b.WriteRune('l')
 	}
 	return b.String()
 }
